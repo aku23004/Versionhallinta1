c2af11dcfba11260f7405bfaf83ffca113fc4b22
aku23004 <aku23004@student.hamk.fi>
2023-09-27 11:36:33 +0300

    Ohjelman peruspohja
    Alkutekstit ja lukujen määrittäminen muuttujiksi.

---
+++ src/Tulostin.java
@@ -0,0 +1,15 @@
+
+public class Tulostin {
+    public static void main(String[] args) {
+      
+    	String tekija = "Aku";
+        int luku1 = 2;
+        int luku2 = 3;
+    	
+    	System.out.println("Hei olen Tulostin-ohjelma" + "\n" + "Ohjelman tekijä: " + tekija);
+    	
+    	int tulo = luku1 * luku2;
+    	
+    	System.out.println(tulo);
+    }
+}
